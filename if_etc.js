cb38f6333ebec0c29b59ea760268db06d580df30
Furkan Uzhan <furkanuzhanz@gmail.com>
2023-06-19 15:19:48 +0300

    Update if_etc.js

--- if_etc.js
+++ if_etc.js
@@ -1,4 +1,4 @@
-// --------Ders  Ortalaması Bulma------------//
+// --------GPA Calculater------------//
 
 let exam1 = Number(prompt("Enter your first exam score: "))
 let exam2 = Number(prompt("Enter your second exam score: "))
@@ -8,9 +8,9 @@ let final = Number(prompt("Enter your final exam score: "))
 let GPA = (exam1*0.3) + (exam2*0.3) + (final*0.4);
 
 if(GPA>=60){
-    alert("You have passed your lesson!");
-    console.log("You have passed you lesson!");
+    alert("You have passed your lesson!"+ GPA);
+    console.log("You have passed you lesson!" + GPA);
 }else{
-    alert("You have failed your lesson!");
-    console.log("You have failed your lesson!");
+    alert("You have failed your lesson!" + GPA);
+    console.log("You have failed your lesson!" + GPA);
 }
